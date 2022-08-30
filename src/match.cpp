af5b8a34cad89c1b881167b1f4610c522aae5a49
bistcuite <85484922+bistcuite@users.noreply.github.com>
2022-08-30 14:13:51 +0430

    add root delete and write to sda* detector (#2)

--- src/match.cpp
+++ src/match.cpp
@@ -11,9 +11,19 @@ bool match(std::string& str, std::string& pattern) {
 bool check_for_dangerous_command(std::string& bash_script,std::string& filename){
     bool result = false;
 
-    std::string fork_bomb = "^.*\\s*>\\s*/dev/sda\\d*\\s*$";
+    std::string fork_bomb = ""; // TODO
+    std::string delete_root = "^\\s*rm\\s+((-rf)|(-fr)|(-f\\s+--recursive)|(--recursive\\s+-f)|(--force\\s+-r)|(-r\\s+--force)|(--recursive\\s+--force)|(--force\\s+--recursive))\\s+/(home)?\\s*$";
+    std::string write_to_sda = "^.*\\s*>\\s*/dev/sda\\d*\\s*$";
+    // if(match(bash_script, fork_bomb)) {
+    //     std::cout << "Fork bomb detected : " << filename << std::endl;
+    //     result = true;
+    // }
     if(match(bash_script, fork_bomb)) {
-        std::cout << "Fork bomb detected : " << filename << std::endl;
+        std::cout << "Deleting root : " << filename << std::endl;
+        result = true;
+    }
+    if(match(bash_script, write_to_sda)) {
+        std::cout << "Write program output to sda* :" << filename << std::endl;
         result = true;
     }
     // TODO : add more dangerous commands
